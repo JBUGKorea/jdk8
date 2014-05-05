97d4d45f777db2f8f6a99c6bda80ea10b233ced4
oopchoi <oopchoi@gmail.com>
2014-05-05 22:11:45 +0900

    스터디

--- spring31/src/test/java/springbook/learningtest/spring/ioc/ApplicationContextTest1.java
+++ spring31/src/test/java/springbook/learningtest/spring/ioc/ApplicationContextTest1.java
@@ -8,22 +8,21 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertThat;
 
-/**
- * Created with IntelliJ IDEA.
- * User: oopchoi
- * Date: 14. 5. 5.
- * Time: 오후 6:27
- * To change this template use File | Settings | File Templates.
- */
 public class ApplicationContextTest1 {
     @Test
     public void registerBean() {
+        // IoC 컨테이너 생성
         StaticApplicationContext ac = new StaticApplicationContext();
+        // Hello 클래스를 hello1이라는 이름의 싱글톤 빈으로 컨테이너에 등록
         ac.registerSingleton("hello1", Hello.class);
 
+        // IoC 컨테이너에 등록된 hello1이라는 빈을 얻어온다.
         Hello hello1 = ac.getBean("hello1", Hello.class);
+        // setter 호출
         hello1.setName("oopchoi");
+        // 정상적으로 출력되는지 확인
         System.out.println(hello1.sayHello());
+        // null이 아닌지 테스트한다.
         assertThat(hello1, is(notNullValue()));
     }
 }
